85f459f6702d607bbdf7e09d2a7c65debf91e28a
MRVNY <marvinyao@icloud.com>
2021-02-16 23:32:01 +0100

    Test wifi connection

--- Test_wifi/connect.sh
+++ Test_wifi/connect.sh
@@ -1,2 +1,21 @@
-ssid=$(iwlist wlan0 scan | grep ESSID:\"milos_pc_ | cut -d '"' -f 2)
-nmcli device wifi connect $ssid
+#!/bin/bash
+
+echo "Searching..."
+i=1
+cmp=""
+while [ $i -le 5 ]
+do
+  ssid=$(iwlist wlan0 scan | grep ESSID:\"milos_pc_ | cut -d '"' -f 2)
+  
+  if [ "$ssid" = "" ]
+  then 
+    nmcli dev wifi rescan
+    i=$(($i + 1)) 
+  else
+    echo "Connecting..."
+    nmcli device wifi connect $ssid
+    i=10
+  fi
+done
+
+[ $i = 10 ] && echo "Connected to $ssid" || echo "Can't find wifi"
