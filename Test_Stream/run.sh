3d509b830d3bb02815b337fc15cc6e3c2fb260fe
MRVNY <marvinyao@icloud.com>
2021-02-18 23:00:27 +0100

    Creation Test_Stream

---
+++ Test_Stream/run.sh
@@ -0,0 +1,2 @@
+../../../build.sh -p native -A Test_Stream
+../../../out/arsdk-native/staging/native-wrapper.sh ../../../out/arsdk-native/staging/usr/bin/Test_Stream
---
+++ Test_VolRéel_1/run.sh
@@ -0,0 +1,2 @@
+../../../build.sh -p native -A Test_VolRéel_1
+../../../out/arsdk-native/staging/native-wrapper.sh ../../../out/arsdk-native/staging/usr/bin/Test_VolRéel_1
