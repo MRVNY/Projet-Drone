d8f9df93c196fd39b8f92165ec620931faa777f3
Johan-Luca Rossi <johan@MacBook-Pro-de-Johan-Luca.local>
2021-03-23 15:22:42 +0100

    Construction archi

---
+++ Proto/Proto_commun/Pilotage.h
@@ -0,0 +1,47 @@
+#ifndef _PILOTAGE_H_
+#define _PILOTAGE_H_
+
+#define TAG "Protocom"
+
+#define ERROR_STR_LENGTH 2048
+
+#define BEBOP_IP_ADDRESS "192.168.42.1"
+//#define BEBOP_IP_ADDRESS "10.202.0.1"
+#define BEBOP_DISCOVERY_PORT 44444
+
+#define DISPLAY_WITH_MPLAYER 1
+
+#define FIFO_DIR_PATTERN "/tmp/arsdk_XXXXXX"
+#define FIFO_NAME "arsdk_fifo"
+
+void stateChanged (eARCONTROLLER_DEVICE_STATE newState, eARCONTROLLER_ERROR error, void *customData);
+
+void commandReceived (eARCONTROLLER_DICTIONARY_KEY commandKey, ARCONTROLLER_DICTIONARY_ELEMENT_t *elementDictionary, void *customData);
+
+void batteryStateChanged (uint8_t percent);
+
+eARCONTROLLER_ERROR didReceiveFrameCallback (ARCONTROLLER_Frame_t *frame, void *customData);
+
+eARCONTROLLER_ERROR decoderConfigCallback (ARCONTROLLER_Stream_Codec_t codec, void *customData);
+
+int customPrintCallback (eARSAL_PRINT_LEVEL level, const char *tag, const char *format, va_list va);
+
+eARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE getFlyingState(ARCONTROLLER_Device_t *deviceController);
+
+void takeOff(ARCONTROLLER_Device_t *deviceController);
+
+void land(ARCONTROLLER_Device_t *deviceController);
+
+void gaz(ARCONTROLLER_Device_t *deviceController,int valeur,int temps);
+
+void yaw(ARCONTROLLER_Device_t *deviceController,int valeur,int temps);
+
+void roll(ARCONTROLLER_Device_t *deviceController,int valeur,int temps);
+
+void pitch(ARCONTROLLER_Device_t *deviceController,int valeur,int temps);
+
+void setMaxVerticalSpeed(ARCONTROLLER_Device_t *deviceController,int valeur);
+
+void setMaxRotationSpeed(ARCONTROLLER_Device_t *deviceController,int valeur);
+
+#endif 
---
+++ Proto/Proto_commun/run.sh
@@ -0,0 +1,2 @@
+/home/johan/Parrot/build.sh -p native -A Test_Calibration
+/home/johan/Parrot/out/arsdk-native/staging/native-wrapper.sh /home/johan/Parrot/out/arsdk-native/staging/usr/bin/Test_Calibration
