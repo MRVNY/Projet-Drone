4d63ee09f4e73c77ac199e05a1a4dca3e923d987
MelissaLarbi <melissalarbi99@gmail.com>
2021-04-02 18:06:06 +0200

    ajout d etas extrem

--- Décision/decision.c
+++ Décision/decision.c
@@ -1,233 +1,244 @@
 #include<stdio.h> 
 #include<stdlib.h> 
 #include "decision.h"
-#include <pilotage.h> 
+#include <math.h>
 
-//convention pour les etats
-// -1 : proche à droite || un peu en haut 
-// -2 : loin à droite   || tres en haut 
-//  0 : dans l'axe      || dans l'axe 
-//  1 : proche à gauche || un peu en bas 
-//  2 : loin à gauche   || trop en bas 
+// #include "../Pilotage/Pilotage.h"
 
 
+// la convention sur les états 
 
+//  1 : droite
+// -1 : gauche
+//  2 : haut
+// -2 : bas
+//  3 : avant
+// -3 : arriére
+//  4 : tourné à gauche
+// -4 : tourné à droite 
+// ///////////////// pour l'instant 
+//  5  :  ok 
+// -5 : ko
 
-int current_state_y(int y1,int y2){
-    /*
-    Cette fonction renvoie l'etat du dronne selon l'image actuellement recue 
-    y1 étant le deuxième cordonnée de la première abeille et y2 de la deuxième abeille
-    */
- 
-    // on calcule la différence entre les y pour extimer l'état du drone
-
-
-    if ((y1<(TAILLE_Y/8)*2)||(y2<(TAILLE_Y/8)*2)){
-        return 2;
-    }
-    else{
-        if((y1<(TAILLE_Y/8)*3)||(y2<(TAILLE_Y/8)*3)){
-            return 1;
-        }
-        else{
-            if((y1>(TAILLE_Y/8)*6)||(y2>(TAILLE_Y/8)*6)){
-                return -2;
-            }
-            else{
-                if((y1>(TAILLE_Y/8)*5)||(y2>(TAILLE_Y/8)*5)){
-                    return -1;
-                
-                }
-                else{
-                    return 0;
-                }
-            }
-        }
-
-    }
-}
-
-
-
-int current_state_x(int x1,int x2){
-    /*
-    Cette fonction renvoie l'etat du dronne selon l'image actuellement recue 
-    y1 étant le deuxième cordonnée de la première abeille et y2 de la deuxième abeille
-    */
- 
-    // on calcule la différence entre les y pour extimer l'état du drone
-
-    if ((x1<(TAILLE_X/8)*2)||(x2<(TAILLE_X/8)*2)){
-        return -2;
-    }
-    else{
-        if((x1<(TAILLE_X/8)*3)||(x2<(TAILLE_X/8)*3)){
-            return -1;
-        }
-        else{
-            if((x1>(TAILLE_X/8)*6)||(x2>(TAILLE_X/8)*6)){
-                return 2;
-            }
-            else{
-                if((x1>(TAILLE_X/8)*5)||(x2>(TAILLE_X/8)*5)){
-                    return 1;
-                
-                }
-                else{
-                    return 0;
-                }
-            }
-        }
-
-    }
-}
-
-
-/* convention de la sortie: 
-Pour les instruction: 
-    on renvoie un vecteur de type (instruction,intensité) 
-    strafferG/starafferD : 1 pour gauche -1 pour droite 
-    monter/descendre : 2 pour monter , -2 pour descendre 
-    avant/arrière:  3 pour avant ; -3 pour arriére 
-    rotation: 4 pour rotation à guache, -4 pour rotation à droite 
-
+/*
 Pour les intensités: 
-    1 : poiur une commande élémentaire 
-    2: pour le double
+    1 : pour loin un peu 
+    2 : pour tres loin 
+    3 : pour les extrémités
 
 */
 
 
-void cordinate(int **hirondelles){
-    /* re_ordonne le tableau des horondelle de sorte à avoir les 2 premiers pour le mouvement 
-    selon x et les deux dernière pour le mouvement selon y 
+void current_state_y(int **cordonnee,int *tab){  
+    /*
+    Cette fonction renvoie l'etat du dronne selon l'image actuellement recue 
+    y1 étant le deuxième cordonnée de la première abeille et y2 de la deuxième abeille
+    args : 
+        cordonnee: les cordonnee des hirondelle recu sous forme d'une matrice 4*2 
+        info : 
     */
-    int i, j; 
-    int x,y;
-    for(i=0;i<3;i++){
-        for(j=i+1;j<4;j++){
-            if(hirondelles[i][0]<hirondelles[j][0]){
-                x=hirondelles[i][0];
-                y=  hirondelles[i][1];
-                hirondelles[i][0]=hirondelles[j][0];
-                hirondelles[i][1]=hirondelles[j][1];
-                hirondelles[j][0]=x;
-                hirondelles[j][1]=y;
+
+    if((cordonnee[0][1]<(TAILLE_Y/8)*2)||(cordonnee[2][1]<(TAILLE_Y/8)*2)){
+        tab[0]=-1;
+        tab[1]=2;
+
+    }
+    else{
+
+        if((cordonnee[0][1]<(TAILLE_Y/8)*3)||(cordonnee[2][1]<(TAILLE_Y/8)*3)){
+            tab[0]=-1;
+            tab[1]=1;
+        }
+        else{
+            if((cordonnee[1][1]>(TAILLE_Y/8)*6)||(cordonnee[3][1]>(TAILLE_Y/8)*6)){
+                tab[0]=1;
+                tab[1]=2;
             }
-       }
+            else{
+                if((cordonnee[1][1]>(TAILLE_Y/8)*5)||(cordonnee[3][1]>(TAILLE_Y/8)*5)){
+                    tab[0]=1;
+                    tab[1]=1;
+                }
+                else{
+                    tab[0]=0;
+                    tab[1]=0;
+                }
+            }
+
+        }
+    }
+}
+
+void current_state_x(int **cordonnee, int *tab){
+    /*
+    Cette fonction renvoie l'etat du dronne selon l'image actuellement recue 
+    y1 étant le deuxième cordonnée de la première abeille et y2 de la deuxième abeille
+    */
+ 
+    // on calcule la différence entre les y pour extimer l'état du drone
+
+    if ((cordonnee[0][0]<(TAILLE_X/8)*2)||(cordonnee[1][0]<(TAILLE_X/8)*2)){
+        tab[0]=2;
+        tab[1]=2;
+    }
+    else{
+        if((cordonnee[0][0]<(TAILLE_X/8)*3)||(cordonnee[1][0]<(TAILLE_X/8)*3)){
+            tab[0]=2;
+            tab[1]=1;
+        }
+        else{
+            if((cordonnee[2][0]>(TAILLE_X/8)*6)||(cordonnee[3][0]>(TAILLE_X/8)*6)){
+                tab[0]=-2;
+                tab[1]=2;
+            }
+            else{
+                if((cordonnee[2][0]>(TAILLE_X/8)*5)||(cordonnee[3][0]>(TAILLE_X/8)*5)){
+                    tab[0]=-2;
+                    tab[1]=1;
+                
+                }
+                else{
+                    tab[0]=0;
+                    tab[1]=0;
+                }
+            }
+        }
+
     }
 }
 
 
-void commande(int *vecteur, int **cordonnees){
-    /*
-    Cette fonction renvoie la commande à transmettre selon l'état du drone 
-    */
-    cordinate(cordonnees); 
-    int etatx,etaty;
-    etaty = current_state_y(cordonnees[1][1],cordonnees[2][1]);
-    printf("etaty est %d \n",etaty);
-    printf("affichage de l'estimation de la position du drone\n\n ");
-    switch (etaty){
-        case -1 :
-            printf("proche à droite\n");
-            vecteur[0]=1; 
-            vecteur[1]=1; 
-            break;
+int isDefine(int x, int y){
+    return (x>=0 && y>=0);
+}
 
-        case -2 :
-            printf("loin à droite\n");
-            vecteur[0]=1; 
-            vecteur[1]=2; 
-            break;
-        
-        case 1 :
-            printf("proche à gauche\n");
-            vecteur[0]=-1; 
-            vecteur[1]=1; 
-            break;
-        
-        case 2 :
-            printf("loin à gauche\n");
-            vecteur[0]=-1; 
-            vecteur[1]=2; 
-            break;
-        
-        case 0 : 
-            printf(" dans l'axe des X donc on regard selon Y\n ");
-            etatx = current_state_x(cordonnees[0][0],cordonnees[1][0]);
-            switch (etatx){
-                case -1 :
-                    printf("un peu en haut\n");
-                    vecteur[1]=-2; 
-                    vecteur[1]=1; 
-                    break;
 
-                case -2 :
-                    printf("tres en haut\n");
-                    vecteur[0]=-2; 
-                    vecteur[1]=2; 
-                    break;
-                
-                case 1 :
-                    printf("un peu en bas\n");
-                    vecteur[0]=2; 
-                    vecteur[1]=1;
-                    break; 
-                
-                case 2 :
-                    printf("tres en bas\n");
-                    vecteur[0]=2; 
-                    vecteur[1]=2;
-                    break; 
-            }
-        default:
-            printf("\n");
+float distance_euclidienne(int x,int y){
+    return sqrt(pow((x-CENTREIMAGEx),2)+pow((y-CENTREIMAGEy),2));
+}
+
+
+float distanceCentre(int **cordonnee){
+    float som = 0.0;
+    int i;
+    int nb=0;
+    for(i=0;i<TAILLE;i++){
+        if(isDefine(cordonnee[i][0],cordonnee[i][1])){
+            nb++;
+            som += distance_euclidienne(cordonnee[i][0],cordonnee[i][1]);
+        }
     }
+    if(nb!=0){
+        return som/nb;
+    }
+}
 
+
+int evaluation(int **cordonnee,float *DISTANCE_PRECEDENTE){
+    /*
+        renvoie 1 si on se deplace dans le bon sens et 0 sinon 
+    */
+    float distanceActuelle = distanceCentre(cordonnee);
+    int res = (distanceActuelle < *DISTANCE_PRECEDENTE);  // soit 0 ou 1 
+    *DISTANCE_PRECEDENTE = distanceActuelle;
+    return res;
+
+}
+    
+    
+
+int nombre_de_cordonnee_recu(int **vecteur){
+    /*
+        renvoie le nombre de cordonnée renvoyée par la partie imagerie
+    args: 
+        vecteur: un tableau de cordonnée recu de partie imagerie
+        taille: la taille du vecteur 
+    */
+
+    int nb = 0; // au départ le nombre de cordonnée non valide est 0 
+    int i;  // l'indice d'ittération sur le tableau 
+    for(i=0;i<TAILLE;i++){
+        if(vecteur[i][0] >=0 && vecteur[i][1]>=0){ // si les cordonnée sont à -1 i.e que l'hirondelle n'est pas détectée 
+            nb++; 
+        }
+    }
+    return nb; // on renvoie le nombre d'hirondelle détectées 
+}
+
+
+
+void analyseInterpretation(int **cordonnees,int *ETAT_PRECEDENT0,int *ETAT_PRECEDENT1,float *DISTANCE_PRECEDENTE){
+    /*
+        Cette fonction renvoie la commande à transmettre selon l'état du drone 
+        vecteur : le résultat de la partie 
+        cordonnees: les cordonnées des hirondelles 
+    */
+    int *vecteur=(int *)malloc(sizeof(int)*2);
+    int nb_cordonnee = nombre_de_cordonnee_recu(cordonnees);    // on appelle la fonction pour calculer le nombre d'hirondelle recues 
+    if(nb_cordonnee <=2 ){  // i.e on a recu que 2 ou 1 ou 0 hirondelle donc si on a un état précedent on pourai l'utilisé pour savoir si vraiment on est à l'extrémité ou c'est juste une erreur de la partie imageri 
+        vecteur[0]=*ETAT_PRECEDENT0;  
+        vecteur[1]=3;
+    }
+    else{
+        current_state_y(cordonnees,vecteur);
+        if(vecteur[0]==0){
+            current_state_x(cordonnees,vecteur); // la normamlement on appelle directement une fonction de la partie pilotage en donnant en paramètre nos resulat 
+        }
+    }
+    if((vecteur[0]==*ETAT_PRECEDENT0) && (vecteur[1]==*ETAT_PRECEDENT1)){ // je suis tjr dans la mm zone 
+        if(nb_cordonnee!=0){
+            if(evaluation(cordonnees,DISTANCE_PRECEDENTE)==0){ // on envoie un ko 
+                vecteur[0]=-5;  
+                vecteur[1]=0;   
+            }
+            else{ // on envoie un ok 
+                vecteur[0]=5;  
+                vecteur[1]=0; 
+            }
+        }
+    }
+    int k;
+    for(k=0;k<2;k++){
+       printf(" etat: %d \n intensité: %d \n",vecteur[0],vecteur[1]); 
+    }
+    // pilotage(vecteur) 
 }
 
 
 int main(int argc, int *argv){ 
   
+    int  ETAT_PRECEDENT0 = 1000;
+    int  ETAT_PRECEDENT1 = 1000;
+    float DISTANCE_PRECEDENTE = 0.0;
 
-    // on suppose qu'on a recu les 4 hirondelle 
-    int *vecteur=malloc(sizeof(int)*2);
+    int **tab1;
+    tab1[0][0]= -1;
+    tab1[0][1]= -1;
+    tab1[1][0]= 50;
+    tab1[1][1]= 50;
+    tab1[2][0]= -1;
+    tab1[2][1]= -1;
+    tab1[3][0]= 200;
+    tab1[3][1]= 50;
 
-    int **cordonnees=(int **)malloc(sizeof(int*)*4);
-    for(int i=0;i<4;i++){
-        cordonnees[i]=(int*)malloc(sizeof(int)*2);
-    }
-    int i,j;
-    for(i=0;i<4;i++){
-        for (j=0;j<2;j++){
-            scanf("%d",&cordonnees[i][j]);
-        }
 
-    }
-    commande(vecteur, cordonnees);
-    switch(vecteur[0]){
+    // int **tab2 = [[50,200],[50,400],[200,200],[200,400]]; 
+    // int **tab3 = [[50,150],[50,350],[200,150],[1200,150]];
+    // int **tab4 = [[50,200],[50,400],[200,200],[200,400]]; 
+    // int **tab5 = [[50,500],[50,700],[200,500],[200,700]]; 
+    // int **tab6= [[50,800],[50,1000],[200,800],[200,1000]]; 
 
-        case 1 : printf("aller à gauche \n");break;
-        case -1 : printf("aller à droite \n");break;
-        case 2 : printf("monter \n");break;
-        case -2 : printf("descendre \n");break;
-        case 3 :  printf("avancer \n");break;
-        case -3 : printf("reculer \n");break;
-        case 4 : printf("rotation à gauche \n");break;
-        case -4 : printf("rotation à droite \n");break;
-        default : printf("erreur on arrive pas à renvoyer la commande \n");break;
-
-    }
-
-    switch(vecteur[1]){
-
-        case 1 : printf("avec une commande élémentaire \n");break;
-        case 2: printf("avec une double commande élémentaire \n");break;
-
-    }
-
-return 0; 
-    
-
+    printf(" le resultat pour tab1 est :\n");
+    analyseInterpretation(tab1,&ETAT_PRECEDENT0,&ETAT_PRECEDENT1,&DISTANCE_PRECEDENTE);
+    // printf(" le resultat pour tab2 est :\n");
+    // analyseInterpretation(tab2);
+    // printf(" le resultat pour tab3 est :\n");
+    // analyseInterpretation(tab3);
+    // printf(" le resultat pour tab4 est :\n");
+    // analyseInterpretation(tab4);
+    // printf(" le resultat pour tab5 est :\n");
+    // analyseInterpretation(tab5);
+    // printf(" le resultat pour tab6 est :\n");
+    // analyseInterpretation(tab6);
+    return 0;
 }
--- Décision/decision.h
+++ Décision/decision.h
@@ -1,9 +1,15 @@
 // La taille de l'image 
 #define TAILLE_X 1080
 #define TAILLE_Y 1920
+#define CENTREIMAGEx 539.5
+#define CENTREIMAGEy 959.5
+#define TAILLE 4
 
-int current_state_y(int y1,int y2);
-int current_state_x(int x1,int x2);
-void cordinate(int **hirondelles);
-void commande(int *vecteur, int **cordonnees);
-
+void current_state_y(int **cordonnee,int *tab);
+void current_state_x(int **cordonnee, int *tab);
+int isDefine(int x, int y);
+float distance_euclidienne(int x,int y);
+float distanceCentre(int **cordonnee);
+int evaluation(int **cordonnee,float *DISTANCE_PRECEDENTE);
+int nombre_de_cordonnee_recu(int **vecteur);
+void analyseInterpretation(int **cordonnees,int *ETAT_PRECEDENT0,int *ETAT_PRECEDENT1,float *DISTANCE_PRECEDENTE);
