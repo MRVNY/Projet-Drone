5659f4767ff98ae07495cf4a0d0fcbe0e43aff1e
MelissaLarbi <melissalarbi99@gmail.com>
2021-04-09 11:43:25 +0200

    maj commun.h

--- Decision/decision.h
+++ Decision/decision.h
@@ -1,14 +1,16 @@
 // La taille de l'image 
 #define TAILLE_X 1080
 #define TAILLE_Y 1920
-#define CENTREIMAGEx 539.5
-#define CENTREIMAGEy 959.5
-#define TAILLE 4
+// les centre de l'image 
+#define CENTREIMAGEX 539.5
+#define CENTREIMAGEY 959.5
+// la taille de vecteur d'entrée (les hirondelle)
+#define TAILLE 4 
 
-void current_state_y(int **cordonnee,int *tab);
-void current_state_x(int **cordonnee, int *tab);
-int isDefine(int x, int y);
-float distance_euclidienne(int x,int y);
+void current_state_y(int **cordonnee,int *tab); // renvoie la position du drone sur l'axe des y 
+void current_state_x(int **cordonnee, int *tab);  // renvoie la position du drone sur l'a
+int isDefine(int x, int y); //  pour une hirondelle(x,y) verifie si elle est bien renseigné par la partie imagerie
+float distance_euclidienne(int x,int y); 
 float distanceCentre(int **cordonnee);
 int evaluation(int **cordonnee,float *DISTANCE_PRECEDENTE);
 int nombre_de_cordonnee_recu(int **vecteur);
--- commun.h
+++ commun.h
@@ -6,20 +6,39 @@
 #define STOP 0
 #define DROITE 1
 #define GAUCHE -1
-#define HAUT 2
-#define BAS -2
-#define AVANT 3
-#define ARRIERE -3
-#define TOURNEGAUCHE 4
-#define TOURNEDROITE -4
-#define TAKEOFF 6
-#define LAND -6
-//#define OK 5
-//#define KO -5
+#define HAUT 1
+#define BAS -1
+#define AVANT 1
+#define ARRIERE -1
+#define TOURNEGAUCHE -1
+#define TOURNEDROITE 1
+#define GOOD 1
+#define BAD -1
+
 
 // Pour les intensités: 
+#define AXE 0 
 #define CLOSE 1
 #define FAR 2
 #define EXTREME 3
 
-#endif
+// convention sur les index dans le tableau de sortie 
+#define POSITION 0
+#define INTENSITE 1
+#define EVALUATION 2 
+
+// les convention sur les index de la matrice de sortie 
+#define STRAFER 0 
+#define avant_arriere 1
+#define MONTER_DESCENDRE 2  
+#define ROTATION 3 
+
+
+// -1 : pour gauche , en bas  ,rotation à gauche et aarière, ko 
+// 1 : pour droite , haut , rotation à droite et avancer, ok
+
+
+#endif 
+
+
+
