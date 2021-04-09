51e129d14ae6eb327154228e3688736d4af000f2
LinaSai <linasaichi@outlook.com>
2021-04-09 12:46:47 +0200

    correction fonction video reader

--- Bas_niveau/bas_niveau.hpp
+++ Bas_niveau/bas_niveau.hpp
@@ -7,31 +7,40 @@
 #include <vector>
 
 extern "C" {
-    #include "../Décision/decision.h"
-    #include <libavformat/avformat.h>
-    #include <libavcodec/avcodec.h>
-    #include <libavutil/avutil.h>
-    #include <libavutil/pixdesc.h>
-    #include <libavutil/imgutils.h>
-    #include <libswscale/swscale.h>
+#include "../Decision/decision.h"
+#include <libavformat/avformat.h>
+#include <libavcodec/avcodec.h>
+#include <libavutil/avutil.h>
+#include <libavutil/pixdesc.h>
+#include <libavutil/imgutils.h>
+
+#include <libswscale/swscale.h>
 }
 
-#include <opencv2/core.hpp>
-#include <opencv2/highgui.hpp>
-#include<opencv2/opencv.hpp>
+#include <opencv4/opencv2/core.hpp>
+#include <opencv4/opencv2/highgui.hpp>
+#include <opencv4/opencv2/opencv.hpp>
+#include <cmath>
+
 using namespace cv;
+#define VOISINAGE 3
+#define EUCLID 3
 
-class SPoint {       // The class
-    public:       // Access specifier
-    int x;
-    int y;// Attribute (int variable)
-    int somme;
 
-    SPoint() : x(0),y(0),somme(0) {}
+struct SPoint
+{
+   int x, y,somme;
 };
 
-int ** video_reader_process(const char* infile);
+
+int intervalle (int x1,int y1,int x2,int y2);
+
+void voisinage(int voisin,int i, int j,int* S0,int* S1,int* S2,int* S3,cv::Mat image);
+
+int video_reader_process(const char* infile);
+
 void video_reader_close(SwsContext* sws_scaler_ctx, AVFormatContext* av_format_ctx, AVFrame* av_frame, AVFrame* decrame);
+
 void image_processing(cv::Mat image,int*** resultat);
 
 #endif /* bas_niveau_hpp */
