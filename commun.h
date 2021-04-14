c9f55333611cd51c3983c0c8add5882e5154ee18
JRossCode <jlrossi1@yahoo.fr>
2021-04-14 15:45:06 +0200

    Merge commun main et bouchon

--- commun.h
+++ commun.h
@@ -1,17 +1,15 @@
 #ifndef _COMMUN_H_
 #define _COMMUN_H_
 
-// La convention sur les états 
 
+// La taille du vecteur de sortie 
+#define TAILLE_SORTIE 4 
+#define INFO_SORTIE 3
+
+// La convention sur les états 
 #define STOP 0
-#define DROITE 1
-#define GAUCHE -1
-#define HAUT 1
-#define BAS -1
-#define AVANT 1
-#define ARRIERE -1
-#define TOURNEGAUCHE -1
-#define TOURNEDROITE 1
+#define NEGATIF -1 
+#define POSITIF  1
 #define GOOD 1
 #define BAD -1
 
@@ -23,20 +21,19 @@
 #define EXTREME 3
 
 // convention sur les index dans le tableau de sortie 
-#define POSITION 0
+#define POSITION 0 
 #define INTENSITE 1
 #define EVALUATION 2 
 
 // les convention sur les index de la matrice de sortie 
 #define STRAFER 0 
-#define avant_arriere 1
+#define AVANT_ARRIERE 1
 #define MONTER_DESCENDRE 2  
 #define ROTATION 3 
 
 
 // -1 : pour gauche , en bas  ,rotation à gauche et aarière, ko 
-// 1 : pour droite , haut , rotation à droite et avancer, ok
-
+// 1 : pour droite , haut , rotation à droite et avancer, ok 
 
 /*---------------Methodes partagées---------------*/
 void callback(int *state);
