fb7f7060a9ff19b6a0317a38525cc6d06fc2c9af
MelissaLarbi <melissalarbi99@gmail.com>
2021-05-21 21:59:42 +0200

    ajout de commentairs et correction de qlq erreurs

--- Decision/decision.h
+++ Decision/decision.h
@@ -1,3 +1,5 @@
+// **********************************************************************************************************************************************
+
 // La taille de l'image 
 #define TAILLE_X 540
 #define TAILLE_Y 960
@@ -29,29 +31,223 @@
 #define BORNE_AXE_ROTATION 0
 
 
-int is_far_left(int a);
-int is_left(int a);
-int is_far_right(int a);
-int is_right(int a);
-int is_top(int a);
-int is_mid_top(int a);
-int is_bottom(int a);
- int is_mid_bottom(int a);
-void current_state_y(int **cordonnee);
-void current_state_x(int **cordonnee);
-int get_nb_pixel(int **cordonnee);
-void current_state_z(int **cordonnee);
-float calcul_ratio1( int a , int b , int c);
-float calcul_ratio2( int a , int b , int c);
-int is_much_turned(float ratio);
-int is_less_turned(float ratio);
-int get_direction( int a , int b );
-void direction_ratio( int **coordonnee, int *direction, float *ratio );
-void current_state_rotation(int **cordonnee);
+
+// *********************************************** LES FONCTION PARTAGEES  ********************************************************************
+
 void isDefine(int **cordonnee, int *hirondelle_defined, int *nb_hirondelle_valide);
-void calcule_dx_dy(int **cordonnee, float *dx, float *dy);
-int analyseInterpretation_x_y(int **cordonnees);
-float calcule_dr(int **cordonnees);
-int analyseInterpretation_rotation(int **cordonnees);
+/*  une fonction pour verifier les hirondelle qu'on a reçues de la partie imagerie
+    
+    Args : 
+        coordonnees (int[4][2])    : les hirondelles reçues
+        hirondelle_defined (int[4]): le tableau de sortie : 0 pour dire que l'hirondelle n'est pas renseignée et 1 sinon
+        nb_hirondelle_valide (int) : le nombre des hirondelles renseignées
+
+
+*/
+
+
+
+
+// *********************************************** LES FONCTION DE TRAITEMENT DE L'AXE DES Y ********************************************************************
+
+int is_far_left(int a);
+/*
+    Une fonction pour savoir si la position de la mire dans l'image est trop loin à gauche
+    args : 
+        a (int) : la cordonnée selon y d'une hirondelles. 
+    return :
+        int : O si l'image n'est pas trop loin à gauche 
+              1 sinon 
+        
+
+*/
+
+int is_left(int a);
+/*
+    Une fonction pour savoir si la position de la mire dans l'image est proche à gauche
+    args : 
+        a (int) : la cordonnée selon y d'une hirondelles. 
+    return :
+        int : O si l'image n'est pas proche à gauche 
+              1 sinon 
+        
+
+*/
+
+int is_far_right(int a);
+/*
+    Une fonction pour savoir si la position de la mire dans l'image est trop loin à droite
+    args : 
+        a (int) : la cordonnée selon y d'une hirondelles. 
+    return :
+        int : O si l'image n'est pas trop loin à droite
+              1 sinon 
+        
+
+*/
+
+int is_right(int a);
+/*
+    Une fonction pour savoir si la position de la mire dans l'image est proche à droite
+    args : 
+        a (int) : la cordonnée selon y d'une hirondelles. 
+    return :
+        int : O si l'image n'est pas proche à droite
+              1 sinon 
+        
+
+*/
+
+void calcule_dy(int **cordonnee, float *dy);
+/*
+    Une fonction pour evaluer le déplacement sur y 
+    args : 
+        cordonnee(int[4][2]) : la cordonnée reçues. 
+        dy (float *)         : la distance entre le centre de la mire dans limage et le centre de l'image selon y 
+*/
+
+void current_state_y(int **cordonnee);
+/*
+    Une fonction pour estimer la position actuelle du drone selon l'axe des y 
+    Args: 
+        cordonnee(int[4][2]): les hirondelles reçues
+*/
+
+int analyseInterpretation_y(int **cordonnees);
+/*
+    Une fonction pour estimer la position du drone et evaluer son deplacement sur l'axe des y. 
+    Args : 
+    cordonnees(int [4][2])  : les hirondelles reçues
+
+    Return (int): 
+        0: si il est dans l'axe selon y 
+        1 sinon 
+*/
+
+
+
+
+// *********************************************** LES FONCTION DE TRAITEMENT DE L'AXE DES x ********************************************************************
+
+int is_top(int a);
+/*
+    Une fonction pour savoir si la position de la mire dans l'image est trop en haut dans l'image
+    args : 
+        a (int) : la cordonnée selon x d'une hirondelles. 
+    return :
+        int : O si l'image n'est pas trop en haut 
+              1 sinon 
+        
+
+*/
+
+int is_mid_top(int a);
+/*
+
+    Une fonction pour savoir si la position de la mire dans l'image est un peu en haut dans l'image
+    args : 
+        a (int) : la cordonnée selon x d'une hirondelles. 
+    return :
+        int : O si l'image n'est pas un peu en haut 
+              1 sinon 
+        
+
+*/
+
+int is_bottom(int a);
+/*
+    Une fonction pour savoir si la position de la mire dans l'image est trop en bas dans l'image
+    args : 
+        a (int) : la cordonnée selon x d'une hirondelles. 
+    return :
+        int : O si l'image n'est pas trop en bas
+              1 sinon 
+        
+
+*/
+
+int is_mid_bottom(int a);
+/*
+    Une fonction pour savoir si la position de la mire dans l'image est un peu en bas dans l'image
+    args : 
+        a (int) : la cordonnée selon x d'une hirondelles. 
+    return :
+        int : O si l'image n'est pas un peu en bas
+              1 sinon 
+        
+
+*/
+
+void calcule_dx(int **cordonnee, float *dx);
+/*
+    Une fonction pour evaluer le déplacement sur x
+    args : 
+        cordonnee(int[4][2]) : la cordonnée reçues. 
+        dx (float *)         : la distance entre le centre de la mire dans limage et le centre de l'image selon x
+*/
+
+void current_state_x(int **cordonnee);
+/*
+    Une fonction pour estimer la position actuelle du drone selon l'axe des x 
+    Args: 
+        cordonnee(int[4][2]): les hirondelles reçues
+*/
+
+int analyseInterpretation_x(int **cordonnees);
+/*
+    Une fonction pour estimer la position du drone et evaluer son deplacement sur l'axe des x. 
+    Args : 
+    cordonnees(int [4][2])  : les hirondelles reçues
+
+    Return (int): 
+        0: si il est dans l'axe selon x 
+        1 sinon 
+*/
+
+
+
+// *********************************************** LES FONCTION DE TRAITEMENT DE L'AXE DES Z ********************************************************************
+
+int get_nb_pixel(int **cordonnee);
+/*
+    Une fonction pour evaluer le déplacement sur z: solon la différence des pixels
+    args : 
+        cordonnee(int[4][2]) : la cordonnée reçues. 
+
+    Return: 
+        (int) : le nombre de pixels de différence entre deux hirondelles
+
+*/
+
+void current_state_z(int **cordonnee);
+/*
+    Une fonction pour estimer la position actuelle du drone selon l'axe des z
+    Args: 
+        cordonnee(int[4][2]): les hirondelles reçues
+*/
+
 int analyseInterpretation_z(int **cordonnees);
-void analyseInterpretation(int **cordonnees);
+/*
+    Une fonction pour estimer la position du drone et evaluer son deplacement sur l'axe des z. 
+    Args : 
+    cordonnees(int [4][2])  : les hirondelles reçues
+
+    Return (int): 
+        0: si il est dans l'axe selon z
+        1 sinon 
+*/
+
+
+
+
+// *********************************************** LES FONCTION DE TRAITEMENT DE LA ROTATION  ********************************************************************
+
+int is_much_turned(float ratio);
+/*
+    Une fonction pour savoir si le drone est trop tourné 
+    args : 
+        ratio (float) : le rapport de déformation 
+    return :
+        int : O si le drone n'est pas trop tourné 
+              1 sinon 
+*/
